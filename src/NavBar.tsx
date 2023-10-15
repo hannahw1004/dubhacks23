ee024ea1bc85771a98adf30d93717f551b168b37
Eric Ye <yuxuan.smiley@gmail.com>
2023-10-15 01:15:46 -0700

    Add avatar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,14 +1,48 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { MenuUnfoldOutlined, MenuFoldOutlined, UnorderedListOutlined, HomeOutlined, BellOutlined }
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { MenuUnfoldOutlined, MenuFoldOutlined, UnorderedListOutlined, HomeOutlined, BellOutlined, UserOutlined }
     from '@ant-design/icons';
-import { Layout, Menu, Button, theme } from 'antd';
+import { Layout, Menu, Button, Avatar, Popover} from 'antd';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, getUserInfo, logout } from './firebase';
 
 const { Sider, Content } = Layout;
 // const collapsed = false;
 
+const userMenu = (
+    <Button danger type="primary" onClick={() => logout()}>
+        Logout
+    </Button>
+)
+
 const NavBar = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const [user, loading, error] = useAuthState(auth);
+  const [image, setImage] = useState<string|null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading && user) {
+      (async () => {
+        const userInfo = await getUserInfo(user);
+        if (userInfo === null) {
+            navigate("/createprofile");
+        }
+        console.log(userInfo);
+        const avatar = userInfo!.profilePicture;
+        console.log(avatar);
+        const base64 = avatar!.toBase64();
+        const profileImageType = userInfo!.profilePictureType;
+        const profileImageURL = `data:${profileImageType};base64,${base64}`;
+        console.log(profileImageURL);
+        setImage(profileImageURL);
+      })();
+    }
+    if (!user && !loading) {
+      // redirect to home page
+      navigate("/login");
+    }
+  }, [user, loading]);
 
   return (
     <Layout>
@@ -17,17 +51,19 @@ const NavBar = () => {
             collapsible
             collapsed={collapsed}
         >
-            <img src="/logo192.png" alt="logo" style={{width: "90%", padding: "10%"}}/>
             <Menu
                 theme="dark"
                 mode="inline"
                 defaultSelectedKeys={['1']}
                 style={{
                     overflow: 'auto',
-                    height: '100vh',
-                    left: 0
+                    left: 0,
+                    height: "100vh",
+                    display: "flex",
+                    flexDirection: "column"
                 }}
             >
+                <img src="/logo192.png" alt="logo" style={{width: "90%", padding: "10%"}}/>
                 <div style={{width: "100%"}}>
                     <Button
                         type="primary"
@@ -51,6 +87,17 @@ const NavBar = () => {
                     icon={<BellOutlined />}>
                     <Link to="/app/notifications">Notifications</Link>
                 </Menu.Item>
+                <div style={{flexGrow: 1}}/>
+                <div style={{display: "flex", justifyContent: "center", alignItems: "center", marginBottom: "1em"}}>
+                    <Popover content={userMenu}>
+                        {
+                            image !== null ?
+                            <Avatar src={image} size="large" key={0}/> :
+                            <Avatar icon={<UserOutlined/>} size="large" key={1}/>
+                        }
+                    </Popover>
+                    {collapsed ? null : <div style={{marginLeft: "0.5em"}}>{user?.displayName}</div>}
+                </div>
             </Menu>
         </Sider>
         <Content style = {{
--- src/firebase.ts
+++ src/firebase.ts
@@ -103,8 +103,10 @@ export const logInGoogle = (): Promise<status> => {
 export const addProfile = async (form: any): Promise<status> => {
   // adds a new document with the name, profile picture, instagram handle,
   // community, room number, and description
+  const data = await form.profilePicture.file.originFileObj.arrayBuffer();
+  console.log(data);
   const bytes = Bytes.fromUint8Array(
-    new Uint8Array(await form.profilePicture.file.originFileObj.arrayBuffer())
+    new Uint8Array(data)
   );
   try {
     await setDoc(doc(db, "users", auth.currentUser!.uid), {
